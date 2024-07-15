be49a12778938499c23f7d827205153a037629f5
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-07-16 00:13:20 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Player from "./components/Player";
+
 
 function App() {
     return (
         <main className="App">
-            <h1>Tic-Tac-Toe</h1>
+            <div id="game-container">
+                <ol id="players">
+                    <Player name="Player 1" symbol="X" />
+                    <Player name="Player 2" symbol="O" />
+                </ol>
+                GAME BOARD
+            </div>
+            LOG
         </main>
     );
 }
