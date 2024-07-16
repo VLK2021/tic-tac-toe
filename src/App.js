af58c075b28a768038fffa31143ac3beeb0de9d7
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-07-16 19:57:01 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
 
 
 function App() {
@@ -7,10 +8,11 @@ function App() {
         <main className="App">
             <div id="game-container">
                 <ol id="players">
-                    <Player name="Player 1" symbol="X" />
-                    <Player name="Player 2" symbol="O" />
+                    <Player initialName="Player 1" symbol="X" />
+                    <Player initialName="Player 2" symbol="O" />
                 </ol>
-                GAME BOARD
+
+                <GameBoard/>
             </div>
             LOG
         </main>
