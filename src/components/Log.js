5f81e0e6bcaadd9b310a8c5c47bb494289979a3f
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-07-26 10:45:04 +0300

    change styles

--- src/components/Log.js
+++ src/components/Log.js
@@ -1,7 +1,7 @@
 export default function Log({turns}) {
 
 
-    return <ol id="log">
+    return <ol id="log">{turns.length >0 && <span>Information:</span>}
         {turns.map(turn => (
             <li key={`${turn.square.row}${turn.square.col}`}>
                 {turn.player} selected {turn.square.row}, {turn.square.col}
